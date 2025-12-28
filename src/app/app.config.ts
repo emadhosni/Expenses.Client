d99f8fd00925a6522e7f38a390db4942d337e08b
Emad El-Din Hosni <eng.emadhosni@gmail.com>
2025-12-28 22:45:40 +0200

    create interceptor to apply token on http requests

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,14 @@
-import {
-    ApplicationConfig,
-    provideBrowserGlobalErrorListeners,
-    provideZoneChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideBrowserGlobalErrorListeners(),
+        // provideBrowserGlobalErrorListeners(),
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
-        provideHttpClient(),
+        provideHttpClient(withInterceptors([authInterceptor])),
     ],
 };
--- src/app/services/auth/auth.ts
+++ src/app/services/auth/auth.ts
@@ -47,4 +47,8 @@ export class Auth {
     isAuthenticated(): boolean {
         return !!localStorage.getItem('token');
     }
+
+    getToken(): string | null {
+        return localStorage.getItem('token');
+    }
 }
